eb22de0d0f4881828000ccaeb451a57d57629037
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-09-27 17:50:18 +0200

    a

--- Tema 1/Ejercicios Practicos/Ejercicios Procesos/Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Tema 1/Ejercicios Practicos/Ejercicios Procesos/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -8,15 +8,16 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         
         //A) Crear un proceso con ProcessBuilder
-        ProcessBuilder proceso = new ProcessBuilder("bash", "-c", "ps aux | grep 'Z'");
+        ProcessBuilder proceso = new ProcessBuilder("bash", "-c", "ps aux");
         proceso.inheritIO();
         
         try {
             Process p = proceso.start();
             p.waitFor();
             
+            ProcessHandle informacion = p.toHandle();
             //B) Muestra informacion del proceso
-            System.out.println("Informacion del proceso " + p.info());
+            System.out.println("Informacion del proceso " + informacion);
             
             //C) Muestra si el proceso esta Activo
             System.out.println("¿Esta vivo el proceso? " + p.isAlive());
