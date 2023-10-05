eca54dba1a728681ad9b445f86ae63c9f4d85347
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-10-05 14:29:02 +0200

    a

--- Tema 1/Ejercicios Practicos/Act1punto3/src/act1punto3/Act1punto3Palabras.java
+++ Tema 1/Ejercicios Practicos/Act1punto3/src/act1punto3/Act1punto3Palabras.java
@@ -3,42 +3,41 @@ package act1punto3;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class Act1punto3 {
+public class Act1punto3Palabras {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
         ProcessBuilder pb = new ProcessBuilder("wc");
-        
-        String ficheroEntrada;
+
+        String ficheroEntrada = "";
         String ficheroSalida;
         boolean exit = true;
-        
-        do{
+
+        do {
             System.out.println("Dime el nombre del fichero de Entrada");
             ficheroEntrada = sc.nextLine();
-            pb.redirectInput(new File(ficheroEntrada));
-            
-            if(!ficheroEntrada.equals("exit")){
-                System.out.println("Dime el nombre del fichero de Salida");
-                ficheroSalida = sc.nextLine();
-                pb.redirectInput(new File(ficheroSalida));
+            if (!ficheroEntrada.equals("exit")) {
+                pb.redirectInput(new File(ficheroEntrada));
+                if (!ficheroEntrada.equals("exit")) {
+                    System.out.println("Dime el nombre del fichero de Salida");
+                    ficheroSalida = sc.nextLine();
+                    pb.redirectOutput(new File(ficheroSalida));
+                }
             } else {
                 exit = false;
             }
-            
+
             try {
                 Process p = pb.start();
             } catch (IOException ex) {
-                System.out.printf("ERROR: %s", ex.getMessage());
+                System.out.printf("ERROR: %s\n", ex.getMessage());
             }
-            
-        } while(exit);
-      
+
+        } while (exit);
+
     }
-    
+
 }
