058ba91c88fe3dd5247c2f08d94e930896659b85
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-10-21 11:20:08 +0200

    Update Act1punto7.java

--- Tema 1/Ejercicios Practicos/Act1punto7/src/act1punto7/Act1punto7.java
+++ Tema 1/Ejercicios Practicos/Act1punto7/src/act1punto7/Act1punto7.java
@@ -16,29 +16,48 @@ public class Act1punto7 {
             try (var br = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
                 String linea;
                 while ((linea = br.readLine()) != null) {
-                    informacion += linea;
+                    informacion += linea + "\n";
                 }
             }
-            p.waitFor();
 
-            if (informacion.substring(informacion.indexOf("lo: flags")).contains("lo: flags")) {
-                String t = informacion.substring(informacion.indexOf("lo"), informacion.length());
-                String inet = t.substring(t.indexOf("inet"), t.indexOf("netmask"));
-                String netmask = t.substring(t.indexOf("netmask"), t.indexOf("inet6"));
-                ret += "lo[" + inet.substring(inet.indexOf(" ")).trim() + "/" + netmask.substring(netmask.indexOf(" ")).trim() + "]";
+            if (informacion.contains("enp4s0:")) {
+                ret += "enp4s0 [activa]\n";
+            } else {
+                ret += "enp4s0 [no activa]\n";
             }
-            
-            
-            System.out.println(informacion);
 
+            if (informacion.contains("enp0s3")) {
+                if (informacion.contains("enp0s3:")) {
+                    String enp0s3 = informacion.substring(informacion.indexOf("enp0s3: "), informacion.length());
+                    String inet = enp0s3.substring(enp0s3.indexOf("inet"), enp0s3.indexOf("netmask"));
+                    String netmask = enp0s3.substring(enp0s3.indexOf("netmask"), enp0s3.indexOf("broadcast"));
+                    ret += "enp0s3 [" + inet.substring(inet.indexOf(" ")).trim() + "/" + netmask.substring(netmask.indexOf(" ")).trim() + "]\n";
+                }
+            }
+
+            if (informacion.contains("lo: flags")) {
+                String lo = informacion.substring(informacion.indexOf("lo:"), informacion.length());
+                String inet = lo.substring(lo.indexOf("inet"), lo.indexOf("netmask"));
+                String netmask = lo.substring(lo.indexOf("netmask"), lo.indexOf("inet6"));
+                ret += "lo [" + inet.substring(inet.indexOf(" ")).trim() + "/" + netmask.substring(netmask.indexOf(" ")).trim() + "]\n";
+            }
+
+            if (informacion.contains("wlp3s0:")) {
+                String wlp3s0 = informacion.substring(informacion.indexOf("wlp3s0:"), informacion.length());
+                String inet = wlp3s0.substring(wlp3s0.indexOf("inet"), wlp3s0.indexOf("netmask"));
+                String netmask = wlp3s0.substring(wlp3s0.indexOf("netmask"), wlp3s0.indexOf("broadcast"));
+                ret += "wlp3s0 [" + inet.substring(inet.indexOf(" ")).trim() + "/" + netmask.substring(netmask.indexOf(" ")).trim() + "]";
+            } else {
+                ret += "wlp3s0 [no activa]";
+            }
+
+            p.waitFor();
 
             System.out.println(ret);
 
         } catch (IOException | InterruptedException ex) {
             System.out.printf("ERROR: %s\n", ex.getMessage());
         }
-
-        System.out.println(ret);
 
     }
 
